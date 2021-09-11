9104c2971344e8b023904e91184e607fae31bbf6
Kristin <kristinponsonby@gmail.com>
2021-09-11 13:15:08 -0400

    Add create action in Recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,20 @@
 class RecipesController < ApplicationController
 
+    # corresponds to fetch call 1
     def index
         recipes = Recipe.all
         render json: recipes
+    end
+    # corresponds to fetch call 2
+    def create
+        recipe = Recipe.create(recipe_params)
+        render json: recipe
+    end
+    
+    private
+
+    def recipe_params
+        params.require(:recipe).permit(:name, :category, :ingredients, :image_url)
     end
 
 end
